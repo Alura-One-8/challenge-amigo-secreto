22d80d64b7d10e7b5e236adbbca0c98d1260f9d8
Patricio Pilco <pattopilco@gmail.com>
2025-02-09 12:02:20 -0500

    completar challenge

---
+++ README.md
@@ -0,0 +1,20 @@
+<em> # Amigo Secreto </em>
+
+![Badge en Desarollo](https://img.shields.io/badge/STATUS-EN%20DESAROLLO-green)
+
+
+## :hammer:Funcionalidades del proyecto
+
+- `Funcionalidad 1`: Ingresar el nombre de un amigo
+- `Funcionalidad 2`: Sorterar el nombre de un listado de amigos
+
+
+## 📁 Acceso al proyecto
+
+Puedes acceder al proyecto en el siguiente enlace: [Amigo Secreto](https://github.com/Alura-One-8/challenge-amigo-secreto)
+
+
+## Autores
+
+| [<img src="https://avatars.githubusercontent.com/u/3297990?v=4" width=115><br><sub>Patricio Pilco</sub>](https://github.com/patriciopilco) |
+| :---: |
--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+
+    //Crear Amigo
+    let amigo = document.getElementById("amigo").value;
+    if (amigo === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    } else  if (amigos.includes(amigo)) {
+        alert("El amigo ya fue agregado");
+        return;
+    }
+    amigos.push(amigo);
+    document.getElementById("amigo").value = "";
+
+    // Agregar amigo a la lista
+    let listaAmigos = document.getElementById("listaAmigos");
+    let htmlAmigo = document.createElement("li");
+    htmlAmigo.appendChild(document.createTextNode(amigo));
+    listaAmigos.appendChild(htmlAmigo);
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("Por favor, agregue al menos un amigo.");
+        return;
+    }
+    let amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+    document.getElementById("resultado").innerHTML = amigoSorteado;
+}
